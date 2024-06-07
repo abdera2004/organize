38b87d795e49faeb9883a5b51ee9c5f9d2382d73
abdera2004 <ruanpereira405@gmail.com>
2024-06-07 17:34:10 -0300

    criando header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { View, StyleSheet, Text, StatusBar, Pressable } from "react-native";
+import { useFonts, Inter_400Regular } from "@expo-google-fonts/inter";
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight+22 : 64;
 
 export default function Header() {
+
+    useFonts({
+        Inter_400Regular,
+    });
+
     return (
         <View style={styles.container}>
             <View style={styles.conteudo}>
-
+                <Text style={styles.texto}>oioioioioio</Text>
             </View>
         </View>
     )
@@ -15,7 +21,18 @@ export default function Header() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#1A1717',
+        backgroundColor: 'red',
         paddingTop: statusBarHeight,
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+        borderBottomWidth: 2,
+        borderBottomColor: 'red',
+        width: '100%',
+        alignItems: 'center'
+    },
+    texto: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white'
     }
 })
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import { View, Text, Pressable } from "react-native";
 import styles from '../../../styles/styles';
+import Header from '../../../src/components/Header';
 
 export default function Home({navigation}) {
   return (
